9c98cf04a2c53feddd68054ef461cbeb61b9d75f
luisgustavom1 <luisgustavomacedo13@gmail.com>
2022-08-22 09:01:57 -0300

    Add index file

---
+++ script.js
@@ -0,0 +1,9 @@
+const linksContainerEl = document.querySelector('#links-container');
+const CSS_DAYS = [3, 8, 23, 98];
+
+CSS_DAYS.forEach(cssDay => {
+  const linkEl = document.createElement('a');
+  linkEl.setAttribute('href', `day${cssDay}/index.html`);
+  linkEl.innerHTML = `Day ${cssDay}`;
+  linksContainerEl.appendChild(linkEl);
+})
